99eb415aec9987464977615531317f5d501a1d93
Mikael Larsson <micke.lar@gmail.com>
2022-01-02 23:06:17 +0100

    Tagit bor seedfunktion som inte funkade

--- Exercise 5 - Garage/GarageHandler.cs
+++ Exercise 5 - Garage/GarageHandler.cs
@@ -353,48 +353,6 @@ namespace Exercise_5___Garage
         {
             var seedMethode = 3;
 
-            // Funkar inte här har inte tillgång till arrayen!
-            //if (seedMethode == 1)  // Kan få Null värden mitt i listan
-            //{
-
-            //    vehicles[0] = new Vehicle(regNo: "ABC123") { Make = "SAAB", Model = "V4", NoOfWheel = 4, Color = "Green"/*, RegNo = "ABC123"*/ };
-            //    vehicles[1] = new Vehicle(regNo: "MLB061") { Color = "Blue"/*, RegNo = "MLB061"*/ };
-            //    //vehicles[2] = new Vehicle() { Color = "Blue", RegNo = "MLB061" };
-            //    //vehicles[2] = new Vehicle() { Color = "Blue", RegNo = "MLB062" };
-            //    vehicles[3] = new Vehicle(regNo: "MLB063") { Color = "Blue"/*, RegNo = "MLB063"*/ };
-            //    vehicles[4] = new Airplane(regNo: "SE-VPU") { Make = "Dynamic", Model = "WT-9", NoOfWheel = 3, Color = "Blue"/*, RegNo = "SE-VPU"*/, NumberOfEngines = 1, WingSpan = 9.00M };
-            //    vehicles[5] = new Boat(regNo: "SE6045") { Make = "Swan", Model = "12a", Color = "White", /*RegNo = "SE6045",*/ Lenght = 35, NoOfWheel = 0 };
-            //    vehicles[6] = new Bus(regNo: "BUS101") { Make = "Volvo", Model = "X7", Color = "Blue", /*RegNo = "BUS101",*/ NoOfWheel = 6, NumberOfSeats = 56 };
-            //    vehicles[7] = new Car(regNo: "TEST") { Make = "Volvo", Model = "XC90", Color = "Blue", /*RegNo = "TEST",*/ Fueltype = "E85", NoOfWheel = 4 };
-            //    vehicles[8] = new Motorcycle(regNo: "MC406") { Make = "Honda", Model = "CB-125", Color = "Black", /*RegNo = "MC406",*/ NoOfWheel = 2, CylinderVolume = 125 };
-            //}
-
-            // Funkar inte här har inte tillgång till arrayen!
-            //if (seedMethode == 2) // ev null i slutet av listan (fyller på från början av arrayen)
-            //{
-            //    Vehicle[] seedVehicles = {
-            //        new Vehicle(regNo: "ABC123") { Make = "SAAB", Model = "V4", NoOfWheel = 4, Color = "Green"/*, RegNo = "ABC123"*/ },
-            //        new Vehicle(regNo: "MLB061") { Color = "Blue"/*, RegNo = "MLB061"*/ },
-            //        //new Vehicle() { Color = "Blue", RegNo = "MLB061" },
-            //        //new Vehicle() { Color = "Blue", RegNo = "MLB062" },
-            //        //new Vehicle() { Color = "Blue", RegNo = "MLB063" },
-            //        new Airplane(regNo: "SE-VPU") { Make ="Dynamic",Model = "WT-9", Color = "Blue", /*RegNo = "SE-VPU",*/ NumberOfEngines = 1, WingSpan = 9.00M },
-            //        new Boat(regNo: "SE6045") { Make ="Swan", Model = "12a", Color = "White", /*RegNo = "SE6045",*/ Lenght = 35, NoOfWheel = 0 },
-            //        new Bus(regNo: "BUS101") { Make = "Volvo", Model = "X2000", Color = "Blue", /*RegNo = "BUS101",*/ NoOfWheel = 6, NumberOfSeats = 56 },
-            //        new Car(regNo: "TEST") { Make = "Volvo", Model = "XC90", Color = "Blue",  /*RegNo = "TEST",*/ Fueltype = "E85", NoOfWheel = 4 },
-            //        new Motorcycle(regNo: "MC406") { Make = "Honda", Model = "CB-125", Color = "Blue",/* RegNo = "MC406",*/ NoOfWheel = 2, CylinderVolume = 125 }
-            //    };
-
-            //    //ToDo SeedVehicles Index out of range if capacity < see down
-            //    var i = 0;
-            //foreach (var vehicle in seedVehicles)
-            //{
-            //    if (i >= garage.Capacity)
-            //        break;
-            //    garage.vehicles[i++] = vehicle;
-            //}
-            //}
-
             //Denna funar då den använder funktionen för att lägga till fordon!
             if (seedMethode == 3)// Using the addmethode
             {
